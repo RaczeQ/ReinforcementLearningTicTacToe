bd2dcbabf4ea636e910415b5f4f282f37e1af2a4
Anna Makuś <anm@dsr.com.pl>
2019-04-17 18:36:24 +0200

    Fix bugduring learning process

--- Game/Learnings/QLearning.cs
+++ Game/Learnings/QLearning.cs
@@ -33,28 +33,21 @@ namespace Game.Learnings
                 var board = new Board();
                 LearnFromMoves(board);
             }
-
-            var t = QFunction.TabluarQFunction;
-            var tt = 5;
-                                           
+                
         }
 
-        //CHANGE TO MATRIX AND REFACTOR
 
         public double? LearnFromMoves(Board board)
         {
-            if(board.GetGameState().Item1 == Board.GameState.Finished)
+            if(!(board.GetGameState().Item1 == Board.GameState.InProgress))
             {
                 return GetReward(board);
             }
             else
-            {
+            {      
                 var currentBoard = board.GetBoardCopy();
                 var currentStateQValue = QFunction.TabluarQFunction.Where(s => s.State == currentBoard.GetHashCode()).FirstOrDefault();
                 var index = currentStateQValue.Actions.ToList().IndexOf(currentStateQValue.Actions.Max());
-                var t = GetMoveCoordinatesFromQValue(index);
-
-                var temp = currentBoard.GetAvailableMoves();
                 board.MakeMove(GetMoveCoordinatesFromQValue(index));
                 var nextMoveReward  = LearnFromMoves(board);
 
@@ -63,27 +56,8 @@ namespace Game.Learnings
                     * (DISCOUNT_FACTOR * (nextMoveReward) - currentStateQValue.Actions[index]);
 
                 return QFunction.TabluarQFunction.Where(s => s.State == currentBoard.GetHashCode()).FirstOrDefault().Actions[index];
-            }
-               
+            }              
         }
-
-
-        //TO REFACTOR
-        private void IniitializeQFunctionMatrix()
-        { 
-            QFunctionMatrix = Matrix<double>.Build.Dense(QFunction.TabluarQFunction.Count, (1 + Board.DEFAULT_SIZE * Board.DEFAULT_SIZE), -1);
-            for (int i = 0; i < QFunction.TabluarQFunction.Count; i++)
-            {
-                QFunctionMatrix[i, 0] = QFunction.TabluarQFunction.ElementAt(i).State;
-                for(int j=0; j<(1 + Board.DEFAULT_SIZE * Board.DEFAULT_SIZE); j++)
-                {
-                    QFunctionMatrix[i, j] = QFunction.TabluarQFunction.ElementAt(i).Actions[j - 1] ?? -1;
-                }
-            }
-
-        }
-
-
 
         Tuple<int, int> GetMoveCoordinatesFromQValue(int index)
         {
